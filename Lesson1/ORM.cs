0e8321f268d69702620d0802eafacb3185ed60cd
Gençay Yıldız <gyildizmail@gmail.com>
2022-05-16 02:18:01 +0300

    Add project files.

---
+++ Lesson1/ORM.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Lesson1
+{
+    public class NorthwindDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            ConfigurationManager configuration = new();
+            configuration.AddJsonFile("appsettings.json");
+            optionsBuilder.UseSqlServer($"Server=localhost, 1433;Database=Northwind;User Id=sa;Password={configuration["Password"]}");
+        }
+    }
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string? Title { get; set; }
+        public string? TitleOfCourtesy { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public DateTime? HireDate { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? PostalCode { get; set; }
+        public string? Country { get; set; }
+        public string? HomePhone { get; set; }
+        public string? Extension { get; set; }
+        public string? Notes { get; set; }
+        public int? ReportsTo { get; set; }
+        public string? PhotoPath { get; set; }
+        public string? Region { get; set; }
+    }
+}
---
+++ Lesson1/Program.cs
@@ -0,0 +1,33 @@
+﻿using Lesson1;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+
+
+#region Kod İçinde SQL
+ConfigurationManager configuration = new();
+configuration.AddJsonFile("appsettings.json");
+
+await using SqlConnection connection = new($"Server=localhost, 1433;Database=Northwind;User Id=sa;Password={configuration["Password"]}");
+await connection.OpenAsync();
+
+SqlCommand command = new("Select * from Employees", connection);
+SqlDataReader dr = await command.ExecuteReaderAsync();
+while (await dr.ReadAsync())
+{
+    Console.WriteLine($"{dr["FirstName"]} {dr["LastName"]}");
+}
+await connection.CloseAsync();
+#endregion
+
+#region ORM
+NorthwindDbContext context = new();
+var employeeDatas = await context.Employees.ToListAsync();
+
+#endregion
+
+//ORM'in kullanılmadığı durumlarda nasıl bir yaklaşım sergilenir?
+//ORM'li ve ORM'siz kod mukayesesi
+//İlişkisel eşleştirme nedir?
+//SQL'den arındırılmış kod!
+//Neden ORM kullanmalıyız?
