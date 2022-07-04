4426992705160e4ec77de561b4f30f9834a3fb40
Gençay Yıldız <gyildizmail@gmail.com>
2022-07-04 23:55:59 +0300

    Lesson 9

---
+++ Lesson9/Migrations/20220704202121_mig_1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lesson9.Migrations
+{
+    public partial class mig_1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Urunler",
+                columns: table => new
+                {
+                    UrunID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Urunler", x => x.UrunID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Urunler");
+        }
+    }
+}
---
+++ Lesson9/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+Console.WriteLine();
+public class ETicaretContext : DbContext
+{
+    public DbSet<Urun> Urunler { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost, 1433;Database=ETicaretDB;User ID=SA;Password=1q2w3e4r+!");
+        //Provider
+        //ConnectionString
+        //Lazy Loading 
+        //vb.
+    }
+}
+public class Urun
+{
+    //public int Id { get; set; }
+    //public int ID { get; set; }
+    //public int UrunId { get; set; }
+    public int UrunID { get; set; }
+}
+
+#region OnConfiguring İle Konfigürasyon Ayarlarını Gerçekleştirmek
+//EF Core tool'unu yapılandırmak için kullandığımız bir metottur.
+//Context nesnesinde override edilerek kullanılmaktadır.
+#endregion
+#region Basit Düzeyde Entity Tanımlama Kuralları
+//EF Core, her tablonun default olarak bir primary key kolonu olması gerektiğini kabul eder!
+//Haliyle, bu kolonu temsil eden bir property tanımlamadığımız taktirde hata verecektir!
+#endregion
+#region Tablo Adını Belirleme
+
+#endregion
