9d3ff1ec4a3cb75fbeba12160b40adf4c4b363cb
Gençay Yıldız <gyildizmail@gmail.com>
2022-09-08 23:59:26 +0300

    Lesson 28

---
+++ Backing_Fields/Program.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+BackingFieldDbContext context = new();
+
+var person = await context.Persons.FindAsync(1);
+//Person person2 = new()
+//{
+//    Name = "Person 101",
+//    Department = "Department 101"
+//};
+
+//await context.Persons.AddAsync(person2);
+//await context.SaveChangesAsync();
+
+Console.Read();
+
+#region Backing Fields
+//Tablo içerisindeki kolonları, entity class'ları içerisinde property'ler ile değil field'larla temsil etmemizi sağlayan bir özelliktir.
+//class Person
+//{
+//    public int Id { get; set; }
+//    public string name;
+//    public string Name { get => name.Substring(0, 3); set => name = value.Substring(0, 3); }
+//    public string Department { get; set; }
+//}
+#endregion
+
+#region BackingField Attributes
+//class Person
+//{
+//    public int Id { get; set; }
+//    public string name;
+//    [BackingField(nameof(name))]
+//    public string Name { get; set; }
+//    public string Department { get; set; }
+//}
+#endregion
+
+#region HasField Fluent API
+//Fleunt API'da HasField metodu BackingField özelliğine karşılık gelmektedir.
+//class Person
+//{
+//    public int Id { get; set; }
+//    public string name;
+//    public string Name { get; set; }
+//    public string Department { get; set; }
+//}
+#endregion
+
+#region Field And Property Access
+//EF Core sorgulama sürecinde entity içerisindeki propertyleri ya da field'ları kullanıp kullanmayacağının davranışını bizlere belirtmektedir.
+
+//EF Core, hiçbir ayarlama yoksa varsayılan olarak propertyler üzerinden verileri işler, eğer ki backing field bildiriliyorsa field üzerinden işler yok eğer backing field bildirildiği halde davranış belirtiliyorsa ne belirtilmişse ona göre işlemeyi devam ettirir.
+
+//UsePropertyAccessMode üzerinden davranış modellemesi gerçekleştirilebilir.
+#endregion
+
+#region Field-Only Properties
+//Entitylerde değerleri almak için property'ler yerine metotların kullanıldığı veya belirli alanların hiç gösterilmemesi gerektiği durumlarda(örneğin primary key kolonu) kullanabilir.
+class Person
+{
+    public int Id { get; set; }
+    public string name;
+    public string Department { get; set; }
+
+    public string GetName()
+        => name;
+    public string SetName(string value)
+        => this.name = value;
+}
+#endregion
+
+class BackingFieldDbContext : DbContext
+{
+    public DbSet<Person> Persons { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost, 1433;Database=BaskingFieldDb;User ID=SA;Password=1q2w3e4r+!");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //modelBuilder.Entity<Person>()
+        //    .Property(p => p.Name)
+        //    .HasField(nameof(Person.name))
+        //    .UsePropertyAccessMode(PropertyAccessMode.PreferProperty);
+
+        //Field : Veri erişim süreçlerinde sadece field'ların kullanılmasını söyler. Eğer field'ın kullanılamayacağı durum söz konusu olursa bir exception fırlatır.
+        //FieldDuringConstruction : Veri erişim süreçlerinde ilgili entityden bir nesne oluşturulma sürecinde field'ların kullanılmasını söyler.,
+        //Property : Veri erişim sürecinde sadece propertynin kullanılmasını söyler. Eğer property'nin kullanılamayacağı durum söz konusuysa (read-only, write-only) bir exception fırlatır.
+        //PreferField,
+        //PreferFieldDuringConstruction,
+        //PreferProperty
+
+        modelBuilder.Entity<Person>()
+            .Property(nameof(Person.name));
+    }
+}
