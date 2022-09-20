c3368245f68c016e4f2d3ac95c46e2be6d23561c
Gençay Yıldız <gyildizmail@gmail.com>
2022-09-20 15:12:39 +0300

    Lesson 30

---
+++ EF_Core_Configurations/Program.cs
@@ -0,0 +1,295 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+ApplicationDbContext context = new();
+
+#region EF Core'da Neden Yapılandırmalara İhtiyacımız Olur?
+//Default davranışları yeri geldiğinde geçersiz kılmak ve özelleştirmek isteyebiliriz. Bundan dolayı yapılandırmalara ihtiyacımız olacaktır.
+#endregion
+
+#region OnModelCreating Metodu
+//EF Core'da yapılandırma deyince akla ilk gelen metot OnModelCreating metodudur.
+//Bu metot, DbContext sınıfı içerisinde virtual olarak ayarlanmış bir metottur.
+//Bizler bu metodu kullanarak model'larımızla ilgili temel konfigürasyonel davranışları(Fluent API) sergileyeibliriz.
+//Bir model'ın yaratılışıyla ilgili tüm konfigürasyonları burada gerçekleştirebilmekteyiz.
+
+#region GetEntityTypes
+//EF Core'da kullanılan entity'leri elde etmek, programatik olarak öğrenmek istiyorsak eğer GetEntityTypes fonksiyonunu kullanabiliriz.
+#endregion
+
+#endregion
+
+#region Configurations | Data Annotations & Fluent API
+
+#region Table - ToTable
+//Generate edilecek tablonun ismini belirlememizi sağlayan yapılandırmadır.
+//Ef Core normal şartlarda generate edeceği tablonun adını DbSet property'sinden almaktadır. Bizler eğer ki bunu özelleştirmek istiyorsak Table attribute'unu yahut ToTable api'ını kullanabilriiz.
+#endregion
+
+#region Column - HasColumnName, HasColumnType, HasColumnOrder
+//EF Core'da tabloların kolonları entity sınıfları içerisindeki property'lere karşılık gelmektedir. 
+//Default olarak property'lerin adı kolon adıyken, türleri/tipleri kolon türleridir.
+//Eğer ki generate edilecek kolon isimlerine ve türlerine müdahale etmek sitiyorsak bu konfigürasyon kullanılır.
+#endregion
+
+#region ForeignKey - HasForeignKey
+//İlişkisel tablo tasarımlarında, bağımlı tabloda esas tabloya karşılık gelecek verilerin tutulduğu kolonu foreign key olarak temsil etmekteyiz.
+//EF Core'da foreign key kolonu genellikle Entity Tnaımlama kuralları gereği default yapılanmalarla oluşturulur.
+//ForeignKey Data Annotations Attribute'unu direkt kullanabilirsiniz. Lakin Fluent api ile bu konfigürasyonu yapacaksanız iki entity arasındaki ilişkiyide modellemeniz gerekmektedir. Aksi taktirde fluent api üzerinde HasForeignKey fonksiyonunu kullanamnazsınız!
+#endregion
+
+#region NotMapped - Ignore
+//EF Core, entity sınıfları içerisindeki tüm proeprtyleri default olarak modellenen tabloya kolon şeklinde migrate eder.
+//Bazn bizler entity sınıfları içerisinde tabloda bir kolona karşılık gelmeyen propertyler tanımlamak mecburiyetinde kalabiliriz.
+//Bu property'lerin ef core tarafından kolon olarak map edilmesini istemediğimizi bildirebilmek için NotMapped ya da Ignore kullanabiliriz.
+#endregion
+
+#region Key - HasKey
+//EF Core'da, default convention olarak bir entity'nin içerisinde Id, ID, EntityId, EntityID vs. şeklinde tanımlanan tüm proeprtylere varsayılan olarak primary key constraint uygulanır.
+//Key ya da HasKey yapılanmalarıyla istediğinmiz her hangi bir proeprty'e default convention dışında pk uygulayabiliriz.
+//EF Core'da bir entity içerisinde kesinlikle PK'i temsil edecek olan property bulunmalıdır. Aksi taktirde EF Core migration olutşurken hata verecektir. Eğer ki tablonun PK'i yoksa bunun bildirilmesi gerekir. 
+#endregion
+
+#region Timestamp - IsRowVersion
+//İleride/sonraki derlerde veri tutarlılığı ile ilgili bir ders yapacağız.
+//Bu derste bir satırdaki verinin bütünsel olarak değişikliğini takip etmemizi sağlayacak olan verisyon mantığını konuşuyor olacağız.
+//İşte bir verinin verisyonunu oluşturmamızı sağlayan yapılanma bu konfigürasyonlardır.
+#endregion
+
+#region Required - IsRequired
+//Bir kolonun nullable ya da not null olup olmamasını bu konfigürasyonla belirleyebiliriz.
+//EF Core'da bir property default oalrak not null şeklinde tanımlanır. Eğer ki property'si nullable yapmak istyorsak türü üzerinde ?(nullable) operatörü ile bbildirimde bulunmamız gerekmektedir.
+#endregion
+
+#region MaxLenght | StringLength - HasMaxLength
+//Bir kolonun max karakter sayısını belirlememizi sağlar.
+#endregion
+
+#region Precision - HasPrecision
+//Küsüratlı sayılarda bir kesinlik belirtmemizi ve noktanın hanesini bildirmemizi sağlayan bir yapılandırmadır.
+#endregion
+
+#region Unicode - IsUnicode
+//Kolon içerisinde unicode karakterler kullanılacaksa bu yapılandırmadan istifade edilebilir.
+#endregion
+
+#region Comment - HasComment
+//EF Core üzerinden oluşturulmuş olan veritabanı nesneleri üzerinde bir açıkalama/yorum yapmak istiyorsanız Comment'i kullanblirsiniz.
+#endregion
+
+#region ConcurrencyCheck - IsConcurrencyToken
+//İleride/sonraki derlerde veri tutarlılığı ile ilgili bir ders yapacağız.
+//Bu derste bir satırdaki verinin bütünsel olarak tutarlılığını sağlayacak bir concurrency token yapılanmasından bahsececeğiz.
+#endregion
+
+#region InverseProperty
+//İki entity arasında birden fazla ilişki varsa eğer bu ilişkilerin hangi navigation property üzerinden oılacağını ayarlamamızı sağlayan bir konfigrasyondur.
+#endregion
+
+#endregion
+
+#region Configurations | Fluent API
+
+#region Composite Key
+
+#endregion
+
+#region HasDefaultSchema
+
+#endregion
+
+#region Property
+
+#region HasDefaultValue
+
+#endregion
+
+#region HasDefaultValueSql
+
+#endregion
+
+#endregion
+
+#region HasComputedColumnSql
+
+#endregion
+
+#region HasConstraintName
+
+#endregion
+
+#region HasData
+
+#endregion
+
+#region HasDiscriminator
+
+#endregion
+
+#region HasField
+
+#endregion
+
+#region HasNoKey
+
+#endregion
+
+#region HasIndex
+
+#endregion
+
+#region HasQueryFilter
+
+#endregion
+
+#region HasValue
+
+#endregion
+
+#region DatabaseGenerated - ValueGeneratedOnAddOrUpdate, ValueGeneratedOnAdd, ValueGeneratedNever
+
+#endregion
+#endregion
+
+
+//[Table("Kisiler")]
+class Person
+{
+    //[Key]
+    public int Id { get; set; }
+    //[ForeignKey(nameof(Department))]
+    public int DId { get; set; }
+    //[Column("Adi", TypeName = "metin", Order = 7)]
+    public string Name { get; set; }
+    //[Required()]
+    //[MaxLength(13)]
+    //[StringLength(14)]
+    [Unicode]
+    public string? Surname { get; set; }
+    //[Precision(5, 3)]
+    public decimal Salary { get; set; }
+    //Yazılımsal amaçla oluşturduğum bir property
+    //[NotMapped]
+    //public string Laylaylom { get; set; }
+
+    [Timestamp]
+    //[Comment("Bu şuna yaramaktadır...")]
+    public byte[] RowVersion { get; set; }
+
+    //[ConcurrencyCheck]
+    //public int ConcurrencyCheck { get; set; }
+
+    public DateTime CreatedDate { get; set; }
+    public Department Department { get; set; }
+}
+class Department
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public ICollection<Person> Persons { get; set; }
+}
+class ApplicationDbContext : DbContext
+{
+    //public DbSet<Person> Persons { get; set; }
+    //public DbSet<Department> Departments { get; set; }
+    public DbSet<Flight> Flights { get; set; }
+    public DbSet<Airport> Airports { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        #region GetEntityTypes
+        //var entities = modelBuilder.Model.GetEntityTypes();
+        //foreach (var entity in entities)
+        //{
+        //    Console.WriteLine(entity.Name);
+        //}
+        #endregion
+        #region ToTable
+        //modelBuilder.Entity<Person>().ToTable("aksdmkasmdk");
+        #endregion
+        #region Column
+        //modelBuilder.Entity<Person>()
+        //    .Property(p => p.Name)
+        //    .HasColumnName("Adi")
+        //    .HasColumnType("asldalsd")
+        //    .HasColumnOrder(7);
+        #endregion
+        #region ForeignKey
+        //modelBuilder.Entity<Person>()
+        //    .HasOne(p => p.Department)
+        //    .WithMany(d => d.Persons)
+        //    .HasForeignKey(p => p.DId);
+        #endregion
+        #region Ignore
+        //modelBuilder.Entity<Person>()
+        //    .Ignore(p => p.Laylaylom);
+        #endregion
+        #region Primary Key
+        //modelBuilder.Entity<Person>()
+        //    .HasKey(p => p.Id);
+        #endregion
+        #region IsRowVersion
+        //modelBuilder.Entity<Person>()
+        //    .Property(p => p.RowVersion)
+        //    .IsRowVersion();
+        #endregion
+        #region Required
+        //modelBuilder.Entity<Person>()
+        //    .Property(p => p.Surname).IsRequired();
+        #endregion
+        #region MaxLength
+        //modelBuilder.Entity<Person>()
+        //    .Property(p => p.Surname)
+        //    .HasMaxLength(13);
+        #endregion
+        #region Precision
+        //modelBuilder.Entity<Person>()
+        //    .Property(p => p.Salary)
+        //    .HasPrecision(5, 3);
+        #endregion
+        #region Unicode
+        //modelBuilder.Entity<Person>()
+        //    .Property(p => p.Surname)
+        //    .IsUnicode();
+        #endregion
+        #region Comment
+        //modelBuilder.Entity<Person>()
+        //        .HasComment("Bu tablo şuna yaramaktadır...")
+        //    .Property(p => p.Surname)
+        //        .HasComment("Bu kolon şuna yaramaktadır.");
+        #endregion
+        #region ConcurrencyCheck
+        //modelBuilder.Entity<Person>()
+        //    .Property(p => p.ConcurrencyCheck)
+        //    .IsConcurrencyToken();
+        #endregion
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost, 1433;Database=ApplicationDb;User ID=SA;Password=1q2w3e4r+!");
+    }
+}
+
+public class Flight
+{
+    public int FlightID { get; set; }
+    public int DepartureAirportId { get; set; }
+    public int ArrivalAirportId { get; set; }
+    public string Name { get; set; }
+    public Airport DepartureAirport { get; set; }
+    public Airport ArrivalAirport { get; set; }
+}
+
+public class Airport
+{
+    public int AirportID { get; set; }
+    public string Name { get; set; }
+    [InverseProperty(nameof(Flight.DepartureAirport))]
+    public virtual ICollection<Flight> DepartingFlights { get; set; }
+
+    [InverseProperty(nameof(Flight.ArrivalAirport))]
+    public virtual ICollection<Flight> ArrivingFlights { get; set; }
+}
