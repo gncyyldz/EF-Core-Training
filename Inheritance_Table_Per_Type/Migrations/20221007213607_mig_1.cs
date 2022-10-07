270842b866517c70bd69ca240bef192df3a8afd3
Gençay Yıldız <gyildizmail@gmail.com>
2022-10-08 01:52:51 +0300

    Lesson 37

---
+++ Inheritance_Table_Per_Type/Migrations/20221007213607_mig_1.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Inheritance_Table_Per_Type.Migrations
+{
+    public partial class mig_1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Surname = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    CompanyName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Customers_Persons_Id",
+                        column: x => x.Id,
+                        principalTable: "Persons",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Department = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Employees_Persons_Id",
+                        column: x => x.Id,
+                        principalTable: "Persons",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Technicians",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Branch = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Technicians", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Technicians_Employees_Id",
+                        column: x => x.Id,
+                        principalTable: "Employees",
+                        principalColumn: "Id");
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "Technicians");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Persons");
+        }
+    }
+}
---
+++ Inheritance_Table_Per_Type/Program.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+ApplicationDbContext context = new();
+
+#region Table Per Type (TPT) Nedir?
+//Entitylerin aralarında kalıtımsal ilişkiye sahip olduğu durumlarda her bir türe/entitye/tip/referans karşılık bir tablo generate eden davranıştır.
+//Her generate edilen bu tablolar hiyerarşik düzlemde kendi aralarında birebir ilişkiye sahiptir.
+#endregion
+#region TPT Nasıl Uygulanır?
+//TPT'yi uygulayabilmek için öncelikle entitylerin kendi aralarında olması gereken mantıkta inşa edilmesi gerekmektedir.
+//Entityler DbSet olarak bildirilmelidir.
+//Hiyerarşik olarak aralarında kalıtımsal ilişki olan tüm entityler OnModelCreating fonksiyonunda ToTable metodu ile konfigüre edilmelidir. Böylece EF Core kalıtımsal ilişki olan bu tablolar arasında TPT davranışının olduğunu anlayacaktır.
+#endregion
+#region TPT'de Veri Ekleme
+//Technician technician = new() { Name = "Şuayip", Surname = "Yıldız", Department = "Yazılım", Branch = "Kodlama" };
+//await context.Technicians.AddAsync(technician);
+
+//Customer customer = new() { Name = "Hilmi", Surname = "Celayir", CompanyName = "Çaykur" };
+//await context.Customers.AddAsync(customer);
+//await context.SaveChangesAsync();
+#endregion
+#region TPT'de Veri Silme
+//Employee? silinecek = await context.Employees.FindAsync(3);
+//context.Employees.Remove(silinecek);
+//await context.SaveChangesAsync();
+
+//Person? silinecekPerson = await context.Persons.FindAsync(1);
+//context.Persons.Remove(silinecekPerson);
+//await context.SaveChangesAsync();
+#endregion
+#region TPT'de Veri Güncelleme
+//Technician technician = await context.Technicians.FindAsync(2);
+//technician.Name = "Mehmet";
+//await context.SaveChangesAsync();
+#endregion
+#region TPT'de Veri Sorgulama
+//Employee employee = new() { Name = "Fatih", Surname = "Yavuz", Department = "ABC" };
+//await context.Employees.AddAsync(employee);
+//await context.SaveChangesAsync();
+
+//var technicians = await context.Technicians.ToListAsync();
+//var employees = await context.Employees.ToListAsync();
+
+//Console.WriteLine();
+#endregion
+
+
+
+abstract class Person
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Surname { get; set; }
+}
+class Employee : Person
+{
+    public string? Department { get; set; }
+}
+class Customer : Person
+{
+    public string? CompanyName { get; set; }
+}
+class Technician : Employee
+{
+    public string? Branch { get; set; }
+}
+
+class ApplicationDbContext : DbContext
+{
+    public DbSet<Person> Persons { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Technician> Technicians { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Person>().ToTable("Persons");
+        modelBuilder.Entity<Employee>().ToTable("Employees");
+        modelBuilder.Entity<Customer>().ToTable("Customers");
+        modelBuilder.Entity<Technician>().ToTable("Technicians");
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost, 1433;Database=ApplicationDB;User ID=SA;Password=1q2w3e4r+!;TrustServerCertificate=True");
+    }
+}
