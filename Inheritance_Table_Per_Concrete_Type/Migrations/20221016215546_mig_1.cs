01d64a730cf29dfc88732f801ea49377478faf2d
Gençay Yıldız <gyildizmail@gmail.com>
2022-10-17 01:28:02 +0300

    Lesson 38

---
+++ Inheritance_Table_Per_Concrete_Type/Migrations/20221016215546_mig_1.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InheritanceTablePerConcreteType.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateSequence(
+                name: "PersonSequence");
+
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false, defaultValueSql: "NEXT VALUE FOR [PersonSequence]"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Surname = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CompanyName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false, defaultValueSql: "NEXT VALUE FOR [PersonSequence]"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Surname = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Department = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Technicians",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false, defaultValueSql: "NEXT VALUE FOR [PersonSequence]"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Surname = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Department = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Branch = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Technicians", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Technicians");
+
+            migrationBuilder.DropSequence(
+                name: "PersonSequence");
+        }
+    }
+}
---
+++ Inheritance_Table_Per_Concrete_Type/Program.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+ApplicationDbContext context = new();
+
+#region Table Per Concrete Type (TPC) Nedir?
+//TPC davranışı, kalıtımsal ilişkiye sahip olan entitylerin olduğu çalışmalarda sadece concrete/somut olan entity'lere karşılık bir tablo oluşturacak davranış modelidir.
+//TPC, TPT'nin daha performanslı versiyonudur.
+#endregion
+#region TPC Nasıl Uygulanır?
+//Hiyerarşik düzlemde abstract olan yapılar üzerinden OnModelCreating üzerinden Entity fonskiyonuyla konfigürasyona girip ardından UseTpcMappingStrategy fonksiyonu eşliğinde davranışın TPC olacağını belirleyebiliriz.
+#endregion
+#region TPC'de Veri Ekleme
+//await context.Technicians.AddAsync(new() { Name = "Gençay", Surname = "Yıldız", Branch = "Yazılım", Department = "Yazılım Departmanı" });
+//await context.Technicians.AddAsync(new() { Name = "Mustafa", Surname = "Yıldız", Branch = "Yazılım", Department = "Yazılım Departmanı" });
+//await context.Technicians.AddAsync(new() { Name = "Hilmi", Surname = "Yıldız", Branch = "Yazılım", Department = "Yazılım Departmanı" });
+//await context.SaveChangesAsync();
+#endregion
+#region TPC'de Veri Silme
+//Technician? silinecek = await context.Technicians.FindAsync(2);
+//context.Technicians.Remove(silinecek);
+//await context.SaveChangesAsync();
+#endregion
+#region TPC'de Veri Güncelleme
+//Technician? guncellenecek = await context.Technicians.FindAsync(3);
+//guncellenecek.Name = "Ahmet";
+//await context.SaveChangesAsync();
+#endregion
+#region TPC'de Veri Sorgulama
+//var datas = await context.Technicians.ToListAsync();
+//Console.WriteLine();
+#endregion
+abstract class Person
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Surname { get; set; }
+}
+class Employee : Person
+{
+    public string? Department { get; set; }
+}
+class Customer : Person
+{
+    public string? CompanyName { get; set; }
+}
+class Technician : Employee
+{
+    public string? Branch { get; set; }
+}
+
+class ApplicationDbContext : DbContext
+{
+    public DbSet<Person> Persons { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Technician> Technicians { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //TPT
+        //modelBuilder.Entity<Person>().ToTable("Persons");
+        //modelBuilder.Entity<Employee>().ToTable("Employees");
+        //modelBuilder.Entity<Customer>().ToTable("Customers");
+        //modelBuilder.Entity<Technician>().ToTable("Technicians");
+        modelBuilder.Entity<Person>().UseTpcMappingStrategy();
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost, 1433;Database=ApplicationDB;User ID=SA;Password=1q2w3e4r+!;TrustServerCertificate=True");
+    }
+}
