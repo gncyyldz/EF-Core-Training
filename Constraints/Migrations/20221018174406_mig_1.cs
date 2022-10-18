7daccedb1b52632344a242716c982f49bf7ffcfc
Gençay Yıldız <gyildizmail@gmail.com>
2022-10-19 00:31:00 +0300

    Lesson 39

---
+++ Constraints/Migrations/20221018174406_mig_1.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Constraints.Migrations
+{
+    public partial class mig_1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Blogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BlogName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Url = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blogs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BlogUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    A = table.Column<int>(type: "int", nullable: false),
+                    B = table.Column<int>(type: "int", nullable: false),
+                    BlogId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.Id);
+                    table.CheckConstraint("a_b_check_const", "[A] > [B]");
+                    table.ForeignKey(
+                        name: "FK_Posts_Blogs_BlogId",
+                        column: x => x.BlogId,
+                        principalTable: "Blogs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_BlogId",
+                table: "Posts",
+                column: "BlogId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+
+            migrationBuilder.DropTable(
+                name: "Blogs");
+        }
+    }
+}
---
+++ Constraints/Program.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+ApplicationDbContext context = new();
+
+#region Primary Key Constraint
+
+//Bir kolonu PK constraint ile birincil anahtar yapmak istiyorsak eğer bunun için name convention'dan istifade edebiliriz. Id, ID, EntityNameId, EntityNameID şeklinde tanımlanan tüm propertyler default olarak EF Core tarafından pk constraint olacak şekilde generate edilirler.
+//Eğer ki, farklı bir property'e PK özelliğini atamak istiyorsan burada HasKey Fluent API'ı yahut Key attribute'u ile bu bildirimi iradeli bir şekilde yapmak zorundasın.
+
+#region HasKey Fonksiyonu
+
+#endregion
+#region Key Attribute'u
+
+#endregion
+#region Alternate Keys - HasAlternateKey
+//Bir entity içerisinde PK'e ek olarak her entity instance'ı için alternatif bir benzersiz tanımlayıcı işlevine sahip olan bir key'dir.
+#endregion
+#region Composite Alternate Key
+
+#endregion
+
+#region HasName Fonksiyonu İle Primary Key Constraint'e İsim Verme
+
+#endregion
+#endregion
+
+#region Foreign Key Constraint
+
+#region HasForeignKey Fonksiyonu
+
+#endregion
+#region ForeignKey Attribute'u
+
+#endregion
+#region Composite Foreign Key
+
+#endregion
+
+#region Shadow Property Üzerinden Foreign Key
+
+#endregion
+
+#region HasConstraintName Fonksiyonu İle Primary Key Constraint'e İsim Verme
+
+#endregion
+#endregion
+
+#region Unique Constraint
+
+#region HasIndex - IsUnique Fonksiyonları
+
+#endregion
+
+#region Index, IsUnique Attribute'ları
+
+#endregion
+
+#region Alternate Key
+
+#endregion
+#endregion
+
+#region Check Constratint
+
+#region HasCheckConstraint
+
+#endregion
+#endregion
+
+//[Index(nameof(Blog.Url), IsUnique = true)]
+class Blog
+{
+    public int Id { get; set; }
+    public string BlogName { get; set; }
+    public string Url { get; set; }
+
+    public ICollection<Post> Posts { get; set; }
+}
+class Post
+{
+    public int Id { get; set; }
+    //public int BlogId { get; set; }
+    public string Title { get; set; }
+    public string BlogUrl { get; set; }
+    public int A { get; set; }
+    public int B { get; set; }
+
+    public Blog Blog { get; set; }
+}
+
+
+class ApplicationDbContext : DbContext
+{
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<Post> Posts { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //modelBuilder.Entity<Blog>()
+        //    .HasKey(b => b.Id)
+        //    .HasName("ornek");
+        //modelBuilder.Entity<Blog>()
+        //    .HasAlternateKey(b => new { b.Url, b.BlogName });
+        //modelBuilder.Entity<Blog>()
+        //    .Property<int>("BlogForeignKeyId");
+
+        //modelBuilder.Entity<Blog>()
+        //    .HasMany(b => b.Posts)
+        //    .WithOne(b => b.Blog)
+        //    .HasForeignKey("BlogForeignKeyId")
+        //    .HasConstraintName("ornekforeignkey");
+
+        //modelBuilder.Entity<Blog>()
+        //    .HasIndex(b => b.Url)
+        //    .IsUnique();
+        //modelBuilder.Entity<Blog>()
+        //    .HasAlternateKey(b => b.Url);
+
+        modelBuilder.Entity<Post>()
+            .HasCheckConstraint("a_b_check_const", "[A] > [B]");
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost, 1433;Database=ApplicationDB;User ID=SA;Password=1q2w3e4r+!;TrustServerCertificate=True");
+    }
+}
