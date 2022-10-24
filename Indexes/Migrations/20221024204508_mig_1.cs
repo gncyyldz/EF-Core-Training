8afb8263755fb6bffc5ce773ebd2b82bce2d18d2
Gençay Yıldız <gyildizmail@gmail.com>
2022-10-25 00:43:56 +0300

    Lesson 40

---
+++ Indexes/Migrations/20221024204508_mig_1.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Indexes.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    Surname = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Salary = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_Name",
+                table: "Employees",
+                column: "Name",
+                filter: "[NAME] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
---
+++ Indexes/Program.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+ApplicationDbContext context = new();
+
+#region Index Nedir?
+//Index, bir sütuna dayalı sorgulamaları daha verimli ve performanslı hale getirmek için kullanılan yapıdır.
+#endregion
+#region Index'leme Nasıl Yapılır?
+//PK, FK ve AK olan kolonlar otomatik olarak indexlenir. 
+
+#region Index Attribute'u
+
+#endregion
+#region HasIndex Metodu
+
+#endregion
+#endregion
+#region Composite Index
+//context.Employees.Where(e => e.Name == "" || e.Surname == "")
+#endregion
+#region Birden Fazla Index Tanımlama
+
+#endregion
+#region Index Uniqueness
+
+#endregion
+#region Index Sort Order - Sıralama Düzeni (EF Core 7.0)
+
+#region AllDescending - Attribute
+//Tüm indexlemelerde descending davranışının bütünsel olarak konfigürasyonunu sağlar.
+#endregion
+#region IsDescending - Attribute
+//Indexleme sürecindeki her bir kolona göre sıralama davranışını hususi ayarlamak istiyorsak kullanılır.
+#endregion
+#region IsDescending Metodu
+
+#endregion
+#endregion
+#region Index Name
+
+#endregion
+#region Index Filter
+
+#region HasFilter Metodu
+
+#endregion
+#endregion
+#region Included Columns
+
+#region IncludeProperties Metodu
+
+#endregion
+#endregion
+
+
+
+//[Index(nameof(Name))]
+//[Index(nameof(Surname))]
+//[Index(nameof(Name), nameof(Surname))]
+//[Index(nameof(Name), AllDescending = true)]
+//[Index(nameof(Name), nameof(Surname), IsDescending = new[] { true, false })]
+//[Index(nameof(Name), Name = "name_index")]
+class Employee
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Surname { get; set; }
+    public int Salary { get; set; }
+}
+
+class ApplicationDbContext : DbContext
+{
+    public DbSet<Employee> Employees { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //modelBuilder.Entity<Employee>()
+        //.HasIndex(x => x.Name);
+        //.HasIndex(x => new { x.Name, x.Surname });
+        //.HasIndex(nameof(Employee.Name), nameof(Employee.Surname));
+        //.HasIndex(x => x.Name)
+        //.IsUnique();
+
+        //modelBuilder.Entity<Employee>()
+        //    .HasIndex(x => x.Name)
+        //    .IsDescending();
+
+        //modelBuilder.Entity<Employee>()
+        //    .HasIndex(x => new { x.Name, x.Surname })
+        //    .IsDescending(true, false);
+
+        //modelBuilder.Entity<Employee>()
+        //    .HasIndex(x => x.Name)
+        //    .HasDatabaseName("name_index");
+
+        //modelBuilder.Entity<Employee>()
+        //    .HasIndex(x => x.Name)
+        //    .HasFilter("[NAME] IS NOT NULL");
+
+        modelBuilder.Entity<Employee>()
+            .HasIndex(x => new { x.Name, x.Surname })
+            .IncludeProperties(x => x.Salary);
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost, 1433;Database=ApplicationDB;User ID=SA;Password=1q2w3e4r+!;TrustServerCertificate=True");
+    }
+}
