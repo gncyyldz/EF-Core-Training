7d228d62e8bcddd015c09d8fafbf6a37f0b7c9a5
Gençay Yıldız <gyildizmail@gmail.com>
2022-11-03 02:58:34 +0300

    Lesson 44

---
+++ Explicit_Loading/Migrations/20221102224435_mig_1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Explicit_Loading.Migrations
+{
+    public partial class mig_1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Regions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Regions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RegionId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Surname = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Salary = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Employees_Regions_RegionId",
+                        column: x => x.RegionId,
+                        principalTable: "Regions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmployeeId = table.Column<int>(type: "int", nullable: false),
+                    OrderDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Orders_Employees_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Regions",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 1, "Ankara" });
+
+            migrationBuilder.InsertData(
+                table: "Regions",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 2, "Yozgat" });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "Name", "RegionId", "Salary", "Surname" },
+                values: new object[,]
+                {
+                    { 1, "Gençay", 1, 1500, "Yıldız" },
+                    { 2, "Mahmut", 2, 1500, "Yıldız" },
+                    { 3, "Rıfkı", 1, 1500, "Yıldız" },
+                    { 4, "Cüneyt", 2, 1500, "Yıldız" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Orders",
+                columns: new[] { "Id", "EmployeeId", "OrderDate" },
+                values: new object[,]
+                {
+                    { 1, 1, new DateTime(2022, 11, 3, 1, 44, 34, 861, DateTimeKind.Local).AddTicks(1041) },
+                    { 2, 1, new DateTime(2022, 11, 3, 1, 44, 34, 861, DateTimeKind.Local).AddTicks(1050) },
+                    { 3, 2, new DateTime(2022, 11, 3, 1, 44, 34, 861, DateTimeKind.Local).AddTicks(1051) },
+                    { 4, 2, new DateTime(2022, 11, 3, 1, 44, 34, 861, DateTimeKind.Local).AddTicks(1052) },
+                    { 5, 3, new DateTime(2022, 11, 3, 1, 44, 34, 861, DateTimeKind.Local).AddTicks(1052) },
+                    { 6, 3, new DateTime(2022, 11, 3, 1, 44, 34, 861, DateTimeKind.Local).AddTicks(1053) },
+                    { 7, 3, new DateTime(2022, 11, 3, 1, 44, 34, 861, DateTimeKind.Local).AddTicks(1054) },
+                    { 8, 4, new DateTime(2022, 11, 3, 1, 44, 34, 861, DateTimeKind.Local).AddTicks(1055) },
+                    { 9, 4, new DateTime(2022, 11, 3, 1, 44, 34, 861, DateTimeKind.Local).AddTicks(1055) },
+                    { 10, 1, new DateTime(2022, 11, 3, 1, 44, 34, 861, DateTimeKind.Local).AddTicks(1056) },
+                    { 11, 2, new DateTime(2022, 11, 3, 1, 44, 34, 861, DateTimeKind.Local).AddTicks(1056) }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_RegionId",
+                table: "Employees",
+                column: "RegionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_EmployeeId",
+                table: "Orders",
+                column: "EmployeeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Regions");
+        }
+    }
+}
---
+++ Explicit_Loading/Program.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+ApplicationDbContext context = new();
+#region Explicit Loading
+
+//Oluşturulan sorguya eklenecek verilerin şartlara bağlı bir şekilde/ihtiyaçlara istinaden yüklenmesini sağlayan bir yaklaşımdır.
+
+//var employee = await context.Employees.FirstOrDefaultAsync(e => e.Id == 2);
+//if (employee.Name == "Gençay")
+//{
+//    var orders = await context.Orders.Where(o => o.EmployeeId == employee.Id).ToListAsync();
+//}
+
+#region Reference
+
+//Explicit Loading sürecinde ilişkisel olarak sorguya eklenmek istenen tablonun navigation propertysi eğer ki tekil bir türse bu tabloyu reference ile sorguya ekleyebilemkteyiz.
+
+//var employee = await context.Employees.FirstOrDefaultAsync(e => e.Id == 2);
+////...
+////...
+////...
+//await context.Entry(employee).Reference(e => e.Region).LoadAsync();
+
+//Console.WriteLine();
+#endregion
+#region Collection
+
+//Explicit Loading sürecinde ilişkisel olarak sorguya eklenmek istenen tablonun navigation propertysi eğer ki çoğul/koleksiyonel bir türse bu tabloyu Collection ile sorguya ekleyebilemkteyiz.
+
+//var employee = await context.Employees.FirstOrDefaultAsync(e => e.Id == 2);
+//...
+//...
+//...
+//await context.Entry(employee).Collection(e => e.Orders).LoadAsync();
+
+//Console.WriteLine();
+#endregion
+
+#region Collection'lar da Aggregate Operatör Uygulamak
+//var employee = await context.Employees.FirstOrDefaultAsync(e => e.Id == 2);
+//...
+//...
+//...
+//var count = await context.Entry(employee).Collection(e => e.Orders).Query().CountAsync();
+Console.WriteLine();
+#endregion
+#region Collection'lar da Filtreleme Gerçekleştirmek
+//var employee = await context.Employees.FirstOrDefaultAsync(e => e.Id == 2);
+////...
+////...
+////...
+//var orders = await context.Entry(employee).Collection(e => e.Orders).Query().Where(q => q.OrderDate.Day == DateTime.Now.Day).ToListAsync();
+#endregion
+#endregion
+
+public class Employee
+{
+    public int Id { get; set; }
+    public int RegionId { get; set; }
+    public string? Name { get; set; }
+    public string? Surname { get; set; }
+    public int Salary { get; set; }
+
+    public List<Order> Orders { get; set; }
+    public Region Region { get; set; }
+}
+public class Region
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public ICollection<Employee> Employees { get; set; }
+}
+public class Order
+{
+    public int Id { get; set; }
+    public int EmployeeId { get; set; }
+    public DateTime OrderDate { get; set; }
+
+    public Employee Employee { get; set; }
+}
+
+
+class ApplicationDbContext : DbContext
+{
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<Region> Regions { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost, 1433;Database=ApplicationDB;User ID=SA;Password=1q2w3e4r+!;TrustServerCertificate=True");
+    }
+}
