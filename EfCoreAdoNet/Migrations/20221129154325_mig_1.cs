df3af2c856e5d36994c00d04033f30533f0f5bfd
Gençay Yıldız <gyildizmail@gmail.com>
2022-11-29 20:14:58 +0300

    Lesson 50

---
+++ EfCoreAdoNet/Migrations/20221129154325_mig_1.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace EfCoreAdoNet.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    PersonId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.PersonId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    OrderId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PersonId = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.OrderId);
+                    table.ForeignKey(
+                        name: "FK_Orders_Persons_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "Persons",
+                        principalColumn: "PersonId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Persons",
+                columns: new[] { "PersonId", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Ayşe" },
+                    { 2, "Hilmi" },
+                    { 3, "Raziye" },
+                    { 4, "Süleyman" },
+                    { 5, "Fadime" },
+                    { 6, "Şuayip" },
+                    { 7, "Lale" },
+                    { 8, "Jale" },
+                    { 9, "Rıfkı" },
+                    { 10, "Muaviye" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Orders",
+                columns: new[] { "OrderId", "Description", "PersonId", "Price" },
+                values: new object[,]
+                {
+                    { 1, "...", 1, 10 },
+                    { 2, "...", 2, 20 },
+                    { 3, "...", 4, 30 },
+                    { 4, "...", 5, 40 },
+                    { 5, "...", 1, 50 },
+                    { 6, "...", 6, 60 },
+                    { 7, "...", 7, 70 },
+                    { 8, "...", 1, 80 },
+                    { 9, "...", 8, 90 },
+                    { 10, "...", 9, 100 },
+                    { 11, "...", 1, 110 },
+                    { 12, "...", 2, 120 },
+                    { 13, "...", 2, 130 },
+                    { 14, "...", 3, 140 },
+                    { 15, "...", 1, 150 },
+                    { 16, "...", 4, 160 },
+                    { 17, "...", 1, 170 },
+                    { 18, "...", 1, 180 },
+                    { 19, "...", 5, 190 },
+                    { 20, "...", 6, 200 },
+                    { 21, "...", 1, 210 },
+                    { 22, "...", 7, 220 },
+                    { 23, "...", 7, 230 },
+                    { 24, "...", 8, 240 },
+                    { 25, "...", 1, 250 },
+                    { 26, "...", 1, 260 },
+                    { 27, "...", 9, 270 },
+                    { 28, "...", 9, 280 },
+                    { 29, "...", 9, 290 },
+                    { 30, "...", 2, 300 },
+                    { 31, "...", 3, 310 },
+                    { 32, "...", 1, 320 },
+                    { 33, "...", 1, 330 },
+                    { 34, "...", 1, 340 },
+                    { 35, "...", 5, 350 },
+                    { 36, "...", 1, 360 },
+                    { 37, "...", 5, 370 },
+                    { 38, "...", 1, 380 },
+                    { 39, "...", 1, 390 },
+                    { 40, "...", 1, 400 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_PersonId",
+                table: "Orders",
+                column: "PersonId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Persons");
+        }
+    }
+}
---
+++ EfCoreAdoNet/Program.cs
@@ -0,0 +1,141 @@
+﻿
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System.Reflection;
+using System.Transactions;
+
+ApplicationDbContext context = new();
+#region Database Property'si
+//Database property'si veritabanını temsil eden ve EF Core'un bazı işlevlerinin detaylarına erişmemizi sağlayan bir propertydir.
+#endregion
+#region BeginTransaction
+//EF Core, transaction yönetimini otomatik bir şekilde kendisi gerçekleştirmektedir. Eğer ki transaction yönetimini manuel olarak anlık ele almak istiyorsak BeginTransaction fonksiyonunu kullanabiliriz.
+
+//IDbContextTransaction transaction = context.Database.BeginTransaction();
+
+#endregion
+#region CommitTransaction
+//EF Core üzerinde yapılan çalışmaların commit edilebilmesi için kullanılan bir fonksiyondur.
+//context.Database.CommitTransaction();
+
+#endregion
+#region RollbackTransaction
+//EF Core üzerinde yapılan çalışmaların rollback edilebilmesi için kullanılan bir fonksiyondur.
+//context.Database.RollbackTransaction();
+#endregion
+#region CanConnect
+//Verilen connection string'e karşılık bağlantı kurulabilir bir veritabanı var mı yok mu bunun bilgisini bool türde veren bir fonksiyondur.
+//bool connect = context.Database.CanConnect();
+//Console.WriteLine(connect);
+#endregion
+#region EnsureCreated
+//EF Core'da tasarlanan veritabanını migration kullanmaksızın, runtime'da yani kod üzerinde veritabanı sunucusuna inşa edebilmek için kullanılan bir fonksiyondur.
+//context.Database.EnsureCreated();
+#endregion
+#region EnsureDeleted
+//İnşa edilmiş veritabanını runtime'da silmemizi sağlayan bir fonksiyondur.
+//context.Database.EnsureDeleted();
+#endregion
+#region GenerateCreateScript
+//Context nesnesinde yapılmış olan veritabanı tasarımı her ne ise ona uygun bir SQL Script'ini string olarak veren metottur.
+//var script = context.Database.GenerateCreateScript();
+//Console.WriteLine(script);
+#endregion
+#region ExecuteSql
+//Veritabanına yapılacak Insert, Update ve Delete sorgularını yazdığımız bir metottur. Bu metot işlevsel olarak alacağı parametreleri SQL Injection saldırılarına karşı korumaktadır. 
+//string name = Console.ReadLine();
+//var result = context.Database.ExecuteSql($"INSERT Persons VALUES('{name}')");
+#endregion
+#region ExecuteSqlRaw
+//Veritabanına yapılacak Insert, Update ve Delete sorgularını yazdığımız bir metottur. Bu metotta ise sorguyu SQL Injection saldırılarına karşı koruma görevi geliştirinin sorumluluğundadır.
+//string name = Console.ReadLine();
+//var result = context.Database.ExecuteSqlRaw($"INSERT Persons VALUES('{name}')");
+#endregion
+#region SqlQuery
+//SqlQuery fonksiyonu her ne kadar erişilebilir olsada artık desteklenememktedir. Bunun yerine DbSet propertysi üzerinden erişilebilen FromSql fonksiyonu gelmiştir/kullanılmaktadır.
+#endregion
+#region SqlQueryRaw
+//SqlQueryRaw fonksiyonu her ne kadar erişilebilir olsada artık desteklenememktedir. Bunun yerine DbSet propertysi üzerinden erişilebilen FromSqlRaw fonksiyonu gelmiştir/kullanılmaktadır.
+#endregion
+#region GetMigrations
+//Uygulamada üretilmiş olan tüm migration'ları runtime'da programatik olarak elde etmemizi sağlayan metottur.
+//var migs = context.Database.GetMigrations();
+//Console.WriteLine();
+#endregion
+#region GetAppliedMigrations
+//Uygulamada migrate edilmiş olan tüm migrationları elde etmemizi sağlayan bir fonksiyondur.
+//var migs = context.Database.GetAppliedMigrations();
+//Console.WriteLine();
+#endregion
+#region GetPendingMigrations
+//Uygulamada migrate edilmemiş olan tüm migrationları elde etmemizi sağlayan bir fonksiyondur.
+//var migs = context.Database.GetPendingMigrations();
+//Console.WriteLine();
+#endregion
+#region Migrate
+//Migration'ları programatik olarak runtime'da migrate etmek için kullanılan bir fonksiyondur.
+//context.Database.Migrate();
+//EnsureCreated fonksiyonu migration'ları kapsamamaktadır. O yüzden migraton'lar içerisinde yapılan çalışmalar ilgili fonksiyonda geçerli olmayacaktır.
+#endregion
+#region OpenConnection
+//Veritabanı bağlantısını manuel açar.
+//context.Database.OpenConnection();
+#endregion
+#region CloseConnection
+//Veritabanı bağlantısını manuel kapatır.
+//context.Database.CloseConnection();
+#endregion
+#region GetConnectionString
+//İlgili context nesnesinin o anda kullandığı connectionstring değeri ne ise onu elde etmenizi sağlar.
+//Console.WriteLine(context.Database.GetConnectionString());
+#endregion
+#region GetDbConnection
+//EF Core'un kullanmış olduğu Ado.NET altyapısının kullandığı DbConnection nesnesini elde etmemizi sağlayan bir fonksiyondur. Yaniiii bizleri Ado.NET kanadına götürür.
+
+//SqlConnection connection = (SqlConnection)context.Database.GetDbConnection();
+//Console.WriteLine();
+#endregion
+#region SetDbConnection
+//Özelleştirilmiş connection nesnelerini EF Core mimarisine dahil etmemizi sağlayan bir fonksiyondur.
+//context.Database.SetDbConnection();
+#endregion
+#region ProviderName Property'si
+//EF Core'un kullanmış olduğu provider neyse onun bilgisini getiren bir proeprty'dir.
+//Console.WriteLine(context.Database.ProviderName);
+#endregion
+public class Person
+{
+    public int PersonId { get; set; }
+    public string Name { get; set; }
+
+    public ICollection<Order> Orders { get; set; }
+}
+public class Order
+{
+    public int OrderId { get; set; }
+    public int PersonId { get; set; }
+    public string Description { get; set; }
+    public int Price { get; set; }
+
+    public Person Person { get; set; }
+}
+class ApplicationDbContext : DbContext
+{
+    public DbSet<Person> Persons { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        modelBuilder.Entity<Person>()
+            .HasMany(p => p.Orders)
+            .WithOne(o => o.Person)
+            .HasForeignKey(o => o.PersonId);
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost, 1433;Database=ApplicationDB;User ID=SA;Password=1q2w3e4r+!;TrustServerCertificate=True");
+    }
+}
