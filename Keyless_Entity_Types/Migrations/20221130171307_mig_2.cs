b4c0f25e686fe4abc51bc2166dd3073898634326
Gençay Yıldız <gyildizmail@gmail.com>
2022-11-30 20:37:53 +0300

    Lesson 51

---
+++ Keyless_Entity_Types/Migrations/20221130171307_mig_2.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KeylessEntityTypes.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($@"
+                    CREATE VIEW vw_PersonOrderCount
+                    AS
+	                    SELECT p.Name, COUNT(*) Count FROM Persons p
+	                    JOIN Orders o 
+		                    ON p.PersonId = o.PersonId
+	                    GROUP By p.Name
+                ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($@"DROP VIEW vw_PersonOrderCount");
+        }
+    }
+}
---
+++ Keyless_Entity_Types/Program.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+ApplicationDbContext context = new();
+
+
+#region Keyless Entity Types
+//Normal entity type'lara ek olarak primary key içermeyen querylere karşı veritabanı sorguları yürütmek için kullanılan bir özelliktir KET.
+
+//Genellikle aggreate operasyonların yapıldığı group by yahut pivot table gibi işlemler neticesinde elde edilen istatistiksel sonuçlar primary key kolonu barındırmazlar. Bizler bu tarz sorguları Keyless Entity Types özelliği ile sanki bir entity'e karşılı geliyormuş gibi çalıştırabiliriz.
+#endregion
+
+#region Keyless Entity Types Tanımlama
+//1. Hangi sorgu olursa olsun EF Core üzerinden bu sorgunun bir entity'e karşılık geliyormuş gibi işleme/execute'a/çalıştırmaya tabi tutulabilmesi için o sorgunun sonucunda bir entity'nin yine de tasarlanması gerekmektedir.
+//2. Bu entity'nin DbSet property'si olarak DbContext nesnesine eklenmesi gerekmektedir.
+//3. Tanımlamış olduğumuz entity'e OnModelCreating fonksiyonu içerisinde girip bunun bir key'i olmadığını bildirmeli (HasNoKey) ve hangi sorgunun çalıştırılacağı da ToView vs. gibi işlemlerle ifade edilmelidir.
+
+//var datas = await context.PersonOrderCounts.ToListAsync();
+//Console.WriteLine();
+#region Keyless Attribute'u
+
+#endregion
+#region HasNoKey Fluent API'ı
+
+#endregion
+#endregion
+#region Keyless Entity Types Özellikleri Nelerdir?
+
+#endregion
+
+public class PersonOrderCount
+{
+    public string Name { get; set; }
+    public int Count { get; set; }
+}
+public class Person
+{
+    public int PersonId { get; set; }
+    public string Name { get; set; }
+
+    public ICollection<Order> Orders { get; set; }
+}
+public class Order
+{
+    public int OrderId { get; set; }
+    public int PersonId { get; set; }
+    public string Description { get; set; }
+    public int Price { get; set; }
+
+    public Person Person { get; set; }
+}
+class ApplicationDbContext : DbContext
+{
+    public DbSet<Person> Persons { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<PersonOrderCount> PersonOrderCounts { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        modelBuilder.Entity<Person>()
+            .HasMany(p => p.Orders)
+            .WithOne(o => o.Person)
+            .HasForeignKey(o => o.PersonId);
+
+        modelBuilder.Entity<PersonOrderCount>()
+            .HasNoKey()
+            .ToView("vw_PersonOrderCount");
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost, 1433;Database=ApplicationDB;User ID=SA;Password=1q2w3e4r+!;TrustServerCertificate=True");
+    }
+}
