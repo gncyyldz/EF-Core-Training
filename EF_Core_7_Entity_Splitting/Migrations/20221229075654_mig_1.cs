2f7957c59a6648deb64ebc6488281b5508e677a5
Gençay Yıldız <gyildizmail@gmail.com>
2022-12-29 13:25:26 +0300

    Lesson 66 - EF Core Entity Splitting

---
+++ EF_Core_7_Entity_Splitting/Migrations/20221229075654_mig_1.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCore7EntitySplitting.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Addresses",
+                columns: table => new
+                {
+                    PersonId = table.Column<int>(type: "int", nullable: false),
+                    Street = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PostCode = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Country = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addresses", x => x.PersonId);
+                    table.ForeignKey(
+                        name: "FK_Addresses_Persons_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "Persons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PhoneNumbers",
+                columns: table => new
+                {
+                    PersonId = table.Column<int>(type: "int", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PhoneNumbers", x => x.PersonId);
+                    table.ForeignKey(
+                        name: "FK_PhoneNumbers_Persons_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "Persons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Addresses");
+
+            migrationBuilder.DropTable(
+                name: "PhoneNumbers");
+
+            migrationBuilder.DropTable(
+                name: "Persons");
+        }
+    }
+}
---
+++ EF_Core_7_Entity_Splitting/Program.cs
@@ -0,0 +1,77 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+
+ApplicationDbContext context = new();
+
+#region Entity Splitting
+//Birden fazla fiziksel tabloyu Entity Framework Core kısmında tek bir entity ile temsil etmemizi sağlayan bir özelliktir.
+#endregion
+#region Örnek
+#region Veri Eklerken
+Person person = new()
+{
+    Name = "Nevin",
+    Surname = "Yıldız",
+    City = "Ankara",
+    Country = "Türkiye",
+    PhoneNumber = "1234567890",
+    PostCode = "1234567890",
+    Street = "..."
+};
+
+//await context.Persons.AddAsync(person);
+//await context.SaveChangesAsync();
+#endregion
+#region Veri Okurken
+person = await context.Persons.FindAsync(2);
+Console.WriteLine();
+#endregion
+#endregion
+public class Person
+{
+    #region Persons Tablosu
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+
+    #endregion
+    #region PhoneNumbers Tablosu
+    public string? PhoneNumber { get; set; }
+    #endregion
+    #region Addresses Tablosu
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string? PostCode { get; set; }
+    public string Country { get; set; }
+    #endregion
+}
+class ApplicationDbContext : DbContext
+{
+    public DbSet<Person> Persons { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Person>(entityBuilder =>
+        {
+            entityBuilder.ToTable("Persons")
+                .SplitToTable("PhoneNumbers", tableBuilder =>
+                {
+                    tableBuilder.Property(person => person.Id).HasColumnName("PersonId");
+                    tableBuilder.Property(person => person.PhoneNumber);
+                })
+                .SplitToTable("Addresses", tableBuilder =>
+                {
+                    tableBuilder.Property(person => person.Id).HasColumnName("PersonId");
+                    tableBuilder.Property(person => person.Street);
+                    tableBuilder.Property(person => person.City);
+                    tableBuilder.Property(person => person.PostCode);
+                    tableBuilder.Property(person => person.Country);
+                });
+        });
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder
+            .UseSqlServer("Server=localhost, 1433;Database=ApplicationDB;User ID=SA;Password=1q2w3e4r+!;TrustServerCertificate=True");
+    }
+}
